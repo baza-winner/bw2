5935cef8f59b79e4d79b97ca549c23e3be4f7c19
Yury Bikuzin <yury.bikuzin@gmail.com>
2018-12-13 10:46:20 +0300

    bw2 in progress

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-GOOS=darwin GOARCH=amd64 go build -o $HOME/bw2/bin/darwin.amd64/bw2 bw2.go && GOOS=linux GOARCH=amd64 go build -o $HOME/bw2/bin/linux.amd64/bw2 bw2.go
+mkdir -p $HOME/bw2/$1/bin/darwin.amd64 && \
+GOOS=darwin GOARCH=amd64 go build -o $HOME/bw2/$1/bin/darwin.amd64/bw2 bw2.go && \
+mkdir -p $HOME/bw2/bin/$1/linux.amd64 && \
+GOOS=linux GOARCH=amd64 go build -o $HOME/bw2/$1/bin/linux.amd64/bw2 bw2.go
--- bw2.go
+++ bw2.go
@@ -1,7 +1,75 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"runtime"
+
+	"github.com/baza-winner/bwcore/ansi"
+	_ "github.com/baza-winner/bwcore/ansi/tags"
+	"github.com/baza-winner/bwcore/bwos"
+	"github.com/urfave/cli"
+)
 
 func main() {
-	fmt.Println("Hello from bw2")
+	app := cli.NewApp()
+
+	app.Usage = ansi.String("Базовая утилита bw-инфраструктуры")
+	app.Commands = []cli.Command{
+		{
+			Name:    "project",
+			Aliases: []string{"p"},
+			Usage:   "Разворачивает проект",
+			Action: func(c *cli.Context) (err error) {
+				if c.NArg() < 1 {
+					bwos.Exit(1, "<ansiErr>ERR:<ansi> Ожидается <ansiVar>Сокращенное-имя-проекта")
+				}
+				var executable string
+				if executable, err = os.Executable(); err != nil {
+					return
+				}
+				bw2BinDir := path.Clean(path.Join(path.Dir(executable), ".."))
+				_ = bw2BinDir
+				// fmt.Println("GOOS: ", runtime.GOOS)
+				// return
+				projShortcut := c.Args()[0]
+				switch projShortcut {
+				case "dip":
+					fmt.Printf(
+						"%s => %s\n",
+						path.Join(bw2BinDir, runtime.GOOS+"."+runtime.GOARCH, "bw2"),
+						path.Join(bw2BinDir, runtime.GOOS+"."+runtime.GOARCH, projShortcut),
+					)
+					if err = os.Symlink(
+						path.Join(bw2BinDir, runtime.GOOS+"."+runtime.GOARCH, "bw2"),
+						path.Join(bw2BinDir, runtime.GOOS+"."+runtime.GOARCH, projShortcut),
+					); err != nil {
+						return
+					}
+				default:
+					bwos.Exit(1, "<ansiErr>ERR:<ansi> Неизвестное <ansiVar>Сокращенное-имя-проекта <ansiVal>%s", projShortcut)
+				}
+				// fmt.Println(c.Args()[0])
+				return
+			},
+			// Subcommands: []cli.Command{
+			// 	{
+			// 		// Category: "Основные команды",
+			// 		Name:  "dip",
+			// 		Usage: ansi.String("Разворачивает проект <ansiVal>dip2"),
+			// 		// Flags: dockerUpFlags,
+			// 		Action: func(c *cli.Context) (err error) {
+
+			// 			return
+			// 		},
+			// 	},
+			// },
+		},
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
