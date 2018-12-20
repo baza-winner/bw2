8350c65fe08e993a766915ce5f38b2b98a63cd9b
Yury Bikuzin <yury.bikuzin@gmail.com>
2018-12-21 00:18:12 +0300

    jld >> jlf

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ else
 	([ -d "$HOME/bw2/$1/data" ] || mkdir -p "$HOME/bw2/$1/data") && \
 	GOOS=darwin GOARCH=amd64 go build -o "$HOME/bw2/$1/bin/darwin.amd64/bw2"  && \
 	GOOS=linux GOARCH=amd64 go build -o "$HOME/bw2/$1/bin/linux.amd64/bw2"  && \
+  rm -f "$HOME/bw2/$1/data/" *.jld && \
 	cp conf.jlf "$HOME/bw2/$1/data" && \
-	cp conf.jld "$HOME/bw2/$1/data" && \
-  cp proj.conf.jld "$HOME/bw2/$1/data" && \
+  cp proj.conf.def.jlf "$HOME/bw2/$1/data" && \
 	true
 fi
